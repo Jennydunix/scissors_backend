5e72b69145747df1388ce445d2fe9e24ce00f860
jennydunix <jennifer.o.odunze@gmail.com>
2023-06-08 21:42:56 +0100

    edited app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ connectDB();
 
 //Middleware
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://scissors.vercel.app/", credentials: true }));
 app.use(cookieParser());
 
 const port = process.env.PORT;
