597a1ed2f55bcaef1fb238a2c39e57c1fcbf25b4
jennydunix <jennifer.o.odunze@gmail.com>
2023-06-08 20:32:18 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import authRoute from "./routes/Auth.js";
+import linkRoute from "./routes/links.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import "dotenv/config";
+import connectDB from "./utils/db.js";
+
+const app = express();
+
+// connect to database
+connectDB();
+
+//Middleware
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cookieParser());
+
+const port = process.env.PORT;
+
+app.get("/health", (req, res) => {
+	res.json({
+		msg: "All Systems are a go",
+	});
+});
+
+app.use("/auth", authRoute);
+app.use("/links", linkRoute);
+
+app.listen(port, () => {
+	console.log("Node is working ");
+});
